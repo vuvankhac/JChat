be616f977504131cc37ba8bff72d18ac1cf07a36
khacdev <khacvv0451@gmail.com>
2016-01-11 09:44:15 +0700

    Setup xcode project

---
+++ JChat/JChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JChat
+//
+//  Created by Jana on 1/11/16.
+//  Copyright © 2016 khacvv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
